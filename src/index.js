30d1e2c00a41535669a94fa93b30e7d0b69b92c0
jipizarroo <jipizarroo@gmail.com>
2020-01-03 11:21:51 -0300

    tiktaktoe vasio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Add css files
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css'; // 4.x
+import '@fortawesome/fontawesome-free/css/all.css'; // 5.x
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
